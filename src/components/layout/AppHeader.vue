5735529d15cfc016ec165ffe280ee369ecd1f993
Alowed7 <kiril10112006@gmail.com>
2026-01-16 12:24:21 +0100

    Create AppHeader.vue

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/">Dashboard</router-link>
+      <router-link to="/subjects">Subjects</router-link>
+      <router-link to="/tasks">Tasks</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+  </header>
+</template>
+
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
