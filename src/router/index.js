2918cee2ececdc1e0307cdbdd961b395904db19c
Alowed7 <kiril10112006@gmail.com>
2026-01-16 12:24:42 +0100

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import DashboardPage from '../pages/DashboardPage.vue'
+import SubjectsPage from '../pages/SubjectsPage.vue'
+import SubjectDetailPage from '../pages/SubjectDetailPage.vue'
+import TasksPage from '../pages/TasksPage.vue'
+import AboutPage from '../pages/AboutPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Dashboard',
+    component: DashboardPage
+  },
+  {
+    path: '/subjects',
+    name: 'Subjects',
+    component: SubjectsPage
+  },
+  {
+    path: '/subjects/:id',
+    name: 'SubjectDetail',
+    component: SubjectDetailPage,
+    props: true
+  },
+  {
+    path: '/tasks',
+    name: 'Tasks',
+    component: TasksPage
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutPage
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
