314d29885f422545d297ed5753bfc04d90d7a483
Alowed7 <kiril10112006@gmail.com>
2026-01-16 12:24:23 +0100

    Create SubjectCard.vue

---
+++ src/components/subjects/SubjectCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <h3>{{ subject.name }}</h3>
+    <p>Semester: {{ subject.semester }}</p>
+
+    <router-link :to="`/subjects/${subject.id}`">
+      View details
+    </router-link>
+
+    <button
+      class="btn"
+      style="margin-top:8px"
+      @click="$emit('delete', subject.id)"
+    >
+      Delete
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SubjectCard',
+  props: {
+    subject: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
