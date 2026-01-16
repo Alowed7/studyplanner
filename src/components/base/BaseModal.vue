d9abc97af77e199fa23915a83a4fd4ee153d124b
Alowed7 <kiril10112006@gmail.com>
2026-01-16 12:23:59 +0100

    Create BaseModal.vue

---
+++ src/components/base/BaseModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="modal" v-if="modelValue">
+    <div class="modal-content">
+      <slot />
+      <BaseButton @click="$emit('update:modelValue', false)">
+        Close
+      </BaseButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseButton from './BaseButton.vue'
+
+export default {
+  name: 'BaseModal',
+  components: { BaseButton },
+  props: {
+    modelValue: Boolean
+  }
+}
+</script>
