e29f7365247872be5cdac8863eb51151c8d413ef
Alowed7 <kiril10112006@gmail.com>
2026-01-16 12:24:01 +0100

    Create ProgressBar.vue

---
+++ src/components/base/ProgressBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="progress">
+    <div
+      class="progress-bar"
+      :style="{ width: percentage + '%' }"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProgressBar',
+  props: {
+    percentage: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
