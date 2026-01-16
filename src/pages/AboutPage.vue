28550b0dbfb8c78983630d59ee16661726263a9c
Alowed7 <kiril10112006@gmail.com>
2026-01-16 12:24:29 +0100

    Create AboutPage.vue

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h1>About</h1>
+    <p>Study Planner project made with Vue 3.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
