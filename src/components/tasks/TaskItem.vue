a4dd2882cb8fcd8a99b1c7b262a7b5ae18619008
Alowed7 <kiril10112006@gmail.com>
2026-01-16 12:24:25 +0100

    Create TaskItem.vue

---
+++ src/components/tasks/TaskItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="task">
+    <input
+      type="checkbox"
+      :checked="task.done"
+      @change="$emit('toggle', task.id)"
+    />
+
+    <span :class="{ done: task.done }">
+      {{ task.title }}
+    </span>
+
+    <button class="btn" @click="$emit('delete', task.id)">
+      ✕
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  props: {
+    task: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
