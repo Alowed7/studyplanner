024dd91534c2e1d619a2976b520fe8d5030493ce
Alowed7 <kiril10112006@gmail.com>
2026-01-16 12:23:43 +0100

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <AppHeader />
+  <main class="container">
+    <router-view />
+  </main>
+</template>
+
+
+<script>
+import AppHeader from '@/components/layout/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
